fbdbd35fe334a39e9e111115d5450a647a5abbd0
nouraabdelfattah <143145113+nouraabdelfattah@users.noreply.github.com>
2024-11-12 01:42:08 +0200

    add

--- backend/Routes/StripeRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const StripeController = require("../Controller/StripeController");
-
-router.post("/create-checkout-session",StripeController.createCheckoutSession); 
-
-module.exports = router
--- backend/Routes/WebHookRoute.js
+++
@@ -1,8 +0,0 @@
-// routes/webhook.js
-const express = require('express');
-const router = express.Router();
-const WebhookController = require('../Controller/WebHookController'); 
-
-router.post('/', WebhookController.handleWebhook);
-
-module.exports = router;
